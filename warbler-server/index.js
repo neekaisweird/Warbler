94c76eff9fab666a360902a9b3bd58b31a9143ce
neeka <neekaishappy@gmail.com>
2020-01-29 15:47:49 -0800

    add user signup and signin functionality

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const errorHandler = require('./handlers/error');
+const authRoutes = require('./routes/auth');
 
 const PORT = 8080;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.use(function(req, res, next) {
   let err = new Error('Not Found');
--- warbler-server/models/index.js
+++ warbler-server/models/index.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/warbler', {
-  keepAlive: true,
-  useMongoClient: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+
+module.exports.User = require('./user');
