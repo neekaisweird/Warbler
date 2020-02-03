92397d34e50794f9f5448639fddb95c1a1b025b2
neeka <neekaishappy@gmail.com>
2020-02-03 15:22:12 -0800

    add Navbar component along with styles

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '../store';
 import { BrowserRouter } from 'react-router-dom';
-
+import Navbar from './Navbar';
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <div>Hello World</div>
+      <div className="onboarding">
+        <Navbar />
+      </div>
     </BrowserRouter>
   </Provider>
 );
