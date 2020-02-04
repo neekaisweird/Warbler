0954c39cc99f3b417af648b3ed84c86bb15fff92
neeka <neekaishappy@gmail.com>
2020-02-03 16:58:01 -0800

    add auth form for signing up or logging in

--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -2,12 +2,32 @@ import React from 'react';
 import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Homepage from '../components/Homepage';
+import AuthForm from '../components/AuthForm';
 
 const Main = () => {
   return (
     <div className="container">
       <Switch>
         <Route exact path="/" render={props => <Homepage {...props} />} />
+        <Route
+          exact
+          path="/login"
+          render={props => (
+            <AuthForm buttonText="Log In" heading="Welcome Back" {...props} />
+          )}
+        />
+        <Route
+          exact
+          path="/signup"
+          render={props => (
+            <AuthForm
+              signUp
+              buttonText="Sign Up"
+              heading="Join Warbler Today"
+              {...props}
+            />
+          )}
+        />
       </Switch>
     </div>
   );
