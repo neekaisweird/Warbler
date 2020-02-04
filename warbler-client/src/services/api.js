442a269a47fd82db1641d1f637a8b8c17e06b2de
neeka <neekaishappy@gmail.com>
2020-02-04 14:01:08 -0800

    add jwt token function

--- warbler-client/src/services/api.js
+++ warbler-client/src/services/api.js
@@ -1,5 +1,14 @@
 import axios from 'axios';
 
+export function setTokenHeader(token) {
+  if (token) {
+    // if we pass a token attach that token to all future requests
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete axios.defaults.headers.common['Authorization'];
+  }
+}
+
 export function apiCall(method, path, data) {
   return new Promise((resolve, reject) => {
     return axios[method](path, data)
